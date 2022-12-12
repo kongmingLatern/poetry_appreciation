0818c222ebb83c6b966c503f95ac6e357fbfa954
凤之兮原 <1577310596@qq.com>
2022-12-12 21:47:52 +0800

    feat: add Contents

---
+++ src/components/Contents.tsx
@@ -0,0 +1,11 @@
+import Cards from './Cards'
+import SearchInput from './SearchInput'
+
+export default function Contents() {
+  return (
+    <>
+      <SearchInput />
+      <Cards />
+    </>
+  )
+}
