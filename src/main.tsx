230578a52a3175c5cfbd596596e41cb156c11c8e
凤之兮原 <1577310596@qq.com>
2022-12-12 16:50:04 +0800

    chore: use unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import './index.css'
 import router from './router'
+import 'uno.css'
+import '@/assets/css/index.css'
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
