57b4a800e480bdb175e8b593684507e47875aaf3
凤之兮原 <1577310596@qq.com>
2022-12-12 16:49:36 +0800

    feat: remove depend and add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
 
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({
+      include: [/\.[tj]sx?$/],
+      dts: './auto-imports.d.ts',
+      imports: ['react', 'react-router-dom'],
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+    Unocss({}),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
