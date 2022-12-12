aa24d17bf98d97a7bbcc18c15e54b5c5511059be
凤之兮原 <1577310596@qq.com>
2022-12-12 21:50:02 +0800

    feat: add router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,8 @@
 import App from '@/App'
 import Home from '@/pages/home/Home'
+import Login from '@/pages/common/Login'
 import { createBrowserRouter } from 'react-router-dom'
+import Register from '@/pages/common/Register'
 
 export default createBrowserRouter([
   {
@@ -10,5 +12,13 @@ export default createBrowserRouter([
   {
     path: '/home',
     element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
   },
 ])
