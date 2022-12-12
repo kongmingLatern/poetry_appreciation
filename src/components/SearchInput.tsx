11633c8cc68b6bce62c6b98bd9c05b0f5e35311c
凤之兮原 <1577310596@qq.com>
2022-12-12 21:49:01 +0800

    feat: add searchInput

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,17 @@
+import { Input } from 'antd'
+
+const { Search } = Input
+const onSearch = (value: string) => console.log(value)
+
+export default function SearchInput() {
+  return (
+    <div className="text-right mr-5 mt-2">
+      <Search
+        placeholder="请输入要查找的古诗"
+        onSearch={onSearch}
+        enterButton
+        className="w-70"
+      />
+    </div>
+  )
+}
