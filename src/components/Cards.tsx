1eacc1249e4913b235beed33f016b311c1eb3735
凤之兮原 <1577310596@qq.com>
2022-12-13 22:50:38 +0800

    enhance card

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,92 +1,55 @@
+import http from '@/api'
 import { Button, Card } from 'antd'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const { Meta } = Card
 
-const cardList = [
-  <Card
-    hoverable
-    className="mx-5 my-5 flex items-center justify-center pl-5 theme_font"
-    cover={
-      <div className="text-center pt-5">
-        <h3 className="text-3xl">绝句</h3>
-        <p className="text-md">作者：123123</p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <Button className="w-50 mb-5" type="primary">
-          评论
-        </Button>
-      </div>
-    }
-  >
-    <Meta
-      title="古诗介绍"
-      description="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    />
-    <Meta
-      title="作者介绍"
-      description="www.instagram.com"
-      className="mt-2"
-    />
-    <Meta
-      title="诗句鉴赏"
-      description="www.instagram.com"
-      className="mt-2"
-    />
-  </Card>,
-  <Card
-    hoverable
-    className="mx-5 my-5 flex items-center justify-center pl-5 theme_font"
-    cover={
-      <div className="text-center pt-5">
-        <h3 className="text-3xl">绝句</h3>
-        <p className="text-md">作者：123123</p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <p className="text-5">
-          两个黄鹂鸣翠柳，一行白鹭上青天
-        </p>
-        <Button className="w-80 mb-5" type="primary">
-          评论
-        </Button>
-      </div>
-    }
-  >
-    <Meta
-      title="古诗介绍"
-      description="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    />
-    <Meta
-      title="作者介绍"
-      description="www.instagram.com"
-      className="mt-2"
-    />
-    <Meta
-      title="诗句鉴赏"
-      description="www.instagram.com"
-      className="mt-2"
-    />
-  </Card>,
-]
-
 export default function Cards(): any {
-  return (
-    <>{new Array(10).fill(cardList).map(item => item)}</>
-  )
+  const [data, setData] = useState<any>([])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    async function getData() {
+      const res = await http.get('/getAllPoem')
+      setData(res.data)
+    }
+    getData()
+  }, [])
+
+  return data.map((item, key) => {
+    return (
+      <Card
+        hoverable
+        className="mx-5 my-5 flex items-center justify-center pl-5 theme_font"
+        key={key}
+        cover={
+          <div className="text-center pt-5">
+            <h3 className="text-3xl">{item.pname}</h3>
+            <p className="text-md">作者：{item.author}</p>
+            {/* 诗句 */}
+            {item.pcontent.map(item => (
+              <p className="text-5">{item}</p>
+            ))}
+            <Button className="w-50 mb-5" type="primary">
+              评论
+            </Button>
+          </div>
+        }
+        onClick={() => navigate('/poem/detail/' + item.pid)}
+      >
+        <Meta title="古诗介绍" description={item.pDesc} />
+        <Meta
+          title="作者介绍"
+          description={item.authorDesc}
+          className="mt-2"
+        />
+        <Meta
+          title="诗句鉴赏"
+          description={item.pAppreciation}
+          className="mt-2"
+        />
+      </Card>
+    )
+  })
 }
