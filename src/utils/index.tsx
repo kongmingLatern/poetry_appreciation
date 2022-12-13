8660149b6792d1fbac5bf713ec0025aaa530063a
凤之兮原 <1577310596@qq.com>
2022-12-13 16:14:54 +0800

    eat: add index.tsx

---
+++ src/utils/index.tsx
@@ -0,0 +1,21 @@
+import {
+  data as UserData,
+  columns as UserColumns,
+} from '@/utils/User'
+import {
+  data as PoemData,
+  columns as PoemColumns,
+} from '@/utils/Poem'
+import {
+  data as CommentData,
+  columns as CommentColumns,
+} from '@/utils/Comment'
+
+export {
+  UserData,
+  UserColumns,
+  PoemData,
+  PoemColumns,
+  CommentData,
+  CommentColumns,
+}
