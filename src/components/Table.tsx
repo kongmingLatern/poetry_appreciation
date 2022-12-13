e076c1b9dd435f7409c1c2c65644f1ee3d81e711
凤之兮原 <1577310596@qq.com>
2022-12-13 22:50:47 +0800

    enhance: enhance Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,21 @@
 import { Table } from 'antd'
+import { useEffect, useState } from 'react'
 
-const App = ({ columns, data }) => (
-  <Table columns={columns} dataSource={data} />
-)
+const App = ({ columns, func }) => {
+  const [result, setResult] = useState([])
+  useEffect(() => {
+    async function getData() {
+      const res = await func()
+      setResult(res)
+    }
+    getData()
+  }, [])
+
+  return (
+    <>
+      <Table columns={columns} dataSource={result} />
+    </>
+  )
+}
 
 export default App
