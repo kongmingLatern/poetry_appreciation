d2c29836a912184a5bcbf0abd32876c0ee062613
凤之兮原 <1577310596@qq.com>
2022-12-13 11:52:33 +0800

    enhance: router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom'
 import App from '@/App'
 import Home from '@/pages/home/Home'
 import Login from '@/pages/common/Login'
-import { createBrowserRouter } from 'react-router-dom'
 import Register from '@/pages/common/Register'
 import Detail from '@/pages/home/Detail'
+import Admin from '@/pages/admin/Admin'
+import Table from '@/components/Table'
+import {
+  data as UserData,
+  columns as UserColumns,
+} from '@/views/admin/User'
 
 export default createBrowserRouter([
   {
@@ -25,5 +31,29 @@ export default createBrowserRouter([
   {
     path: '/poem/detail',
     element: <Detail />,
+  },
+  {
+    path: '/admin',
+    element: <Admin />,
+    children: [
+      {
+        path: 'user',
+        element: (
+          <Table data={UserData} columns={UserColumns} />
+        ),
+      },
+      {
+        path: 'poem',
+        element: (
+          <Table data={UserData} columns={UserColumns} />
+        ),
+      },
+      {
+        path: 'comment',
+        element: (
+          <Table data={UserData} columns={UserColumns} />
+        ),
+      },
+    ],
   },
 ])
