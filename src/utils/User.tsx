974c814f19fa2de3c482f77c0841f119c36c7ce2
凤之兮原 <1577310596@qq.com>
2022-12-14 16:31:51 +0800

    feat: add remove

--- src/utils/User.tsx
+++ src/utils/User.tsx
@@ -1,6 +1,6 @@
 import http from '@/api'
+import { message } from 'antd'
 import { ColumnsType } from 'antd/es/table'
-import { useState } from 'react'
 
 interface UserType {
   uid: string
@@ -8,6 +8,37 @@ interface UserType {
   username: string
   password: string
   isAuth: number
+}
+
+async function remove(type: string, id: string) {
+  let res
+  switch (type) {
+    case 'comment':
+      res = await http.delete('/deleteComment', {
+        params: {
+          cid: id,
+        },
+      })
+      break
+    case 'poem':
+      res = await http.delete('/deletePoem', {
+        params: {
+          pid: id,
+        },
+      })
+      break
+    case 'user':
+      res = await http.delete('/deleteUser', {
+        params: {
+          uid: id,
+        },
+      })
+  }
+  if (res.code === 200) {
+    message.success(res.msg)
+  } else {
+    message.error(res.msg)
+  }
 }
 
 export const columns: ColumnsType<UserType> = [
@@ -36,17 +67,12 @@ export const columns: ColumnsType<UserType> = [
     title: '操作',
     key: 'action',
     render: (_, record) => (
-      <a
-        onClick={() => {
-          console.log(record.uid)
-        }}
-      >
+      <a onClick={() => remove('user', record.uid)}>
         Delete
       </a>
     ),
   },
 ]
-
 
 export async function getData() {
   const res = await http.get('/getAllUser')
