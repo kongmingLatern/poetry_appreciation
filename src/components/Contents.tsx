79c533fa7938fe1e8216d1a56e1cdcdab9615db7
凤之兮原 <1577310596@qq.com>
2022-12-15 20:14:10 +0800

    feat: add Article

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,11 +1,13 @@
+import Article from '@/views/Article'
 import Cards from './Cards'
-import SearchInput from './SearchInput'
+// import SearchInput from './SearchInput'
 
 export default function Contents() {
   return (
     <>
       {/* <SearchInput /> */}
       <Cards />
+      <Article />
     </>
   )
 }
