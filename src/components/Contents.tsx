93bb2ac952dba6baf473db2a737730d45abc2b72
凤之兮原 <1577310596@qq.com>
2022-12-16 10:16:02 +0800

    feat: new Contents layout

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,13 +1,38 @@
-import Article from '@/views/Article'
+import Article from '@/views/home/Article'
+import Culture from '@/views/home/Culture'
+import { Tabs } from 'antd'
 import Cards from './Cards'
 // import SearchInput from './SearchInput'
 
 export default function Contents() {
+  const arr = [
+    {
+      label: '古诗大全',
+      element: <Cards />,
+    },
+    {
+      label: '古诗文化',
+      element: <Culture />,
+    },
+    {
+      label: '古诗文章',
+      element: <Article />,
+    },
+  ]
   return (
     <>
-      {/* <SearchInput /> */}
-      <Cards />
-      <Article />
+      <Tabs
+        defaultActiveKey="1"
+        centered
+        items={arr.map((item, i) => {
+          const id = String(i + 1)
+          return {
+            label: item.label,
+            key: id,
+            children: item.element,
+          }
+        })}
+      />
     </>
   )
 }
