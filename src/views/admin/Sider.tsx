aa66feacd3ecab573263933005dbe97e6ecdd6b5
凤之兮原 <1577310596@qq.com>
2022-12-17 13:57:15 +0800

    change key

--- src/views/admin/Sider.tsx
+++ src/views/admin/Sider.tsx
@@ -1,7 +1,7 @@
 import {
-  UploadOutlined,
+  CommentOutlined,
+  FileTextOutlined,
   UserOutlined,
-  VideoCameraOutlined,
 } from '@ant-design/icons'
 import { Menu, Space } from 'antd'
 import { Link } from 'react-router-dom'
@@ -9,26 +9,26 @@ import { Link } from 'react-router-dom'
 export default function Sider() {
   return (
     <Menu
-      className="h-100"
+      className="h-100vh"
       theme="dark"
       mode="inline"
-      defaultSelectedKeys={['1']}
+      defaultSelectedKeys={['user']}
     >
-      <Menu.Item key={'1'}>
+      <Menu.Item key={'user'}>
         <Space>
           <UserOutlined />
           <Link to={'/admin/user'}>用户管理</Link>
         </Space>
       </Menu.Item>
-      <Menu.Item key={'2'}>
+      <Menu.Item key={'poem'}>
         <Space>
-          <UploadOutlined />
+          <FileTextOutlined />
           <Link to={'/admin/poem'}>诗词管理</Link>
         </Space>
       </Menu.Item>
-      <Menu.Item key={'3'}>
+      <Menu.Item key={'comment'}>
         <Space>
-          <VideoCameraOutlined />
+          <CommentOutlined />
           <Link to={'/admin/comment'}>评论管理</Link>
         </Space>
       </Menu.Item>
